8e91d434039f63b96bd8c4c5e1eedad09a6a702a
Rushab Shah <rushabshah@Rushabs-MacBook-Air.local>
2023-06-06 22:51:00 -0400

    Restructured code. Added source directory and starter code

---
+++ app/parser.py
@@ -0,0 +1,10 @@
+'''
+Author: Rushab Shah
+Code: parser.py
+Purpose: To read PDF files and extract text out of it and return it in a structured format
+'''
+
+def parse():
+    print("Hello!")
+
+parse()
