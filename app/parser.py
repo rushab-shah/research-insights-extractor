0e27c8b75d1a29323e9310520620a7b48a9e3a21
Rushab Shah <rushabshah@Rushabs-MacBook-Air.local>
2023-06-06 23:30:24 -0400

    Added code to read a pdf, parse its text and break it into chunks

--- app/parser.py
+++ app/parser.py
@@ -3,8 +3,56 @@ Author: Rushab Shah
 Code: parser.py
 Purpose: To read PDF files and extract text out of it and return it in a structured format
 '''
+from pdfminer.high_level import extract_text
 
-def parse():
-    print("Hello!")
+RAWDATA_PATH = "../datasources/raw-data"
+PROCESSED_DATA_PATH = "../datasources/processed-data"
+CHUNK_SIZE = 3000
+# SECTIONS = ["ABSTRACT", "INTRODUCTION", "METHODOLOGY", "RESULTS", "DISCUSSION", "CONCLUSIONS"]
 
-parse()
+def parse(filename):
+    """
+    Parse a PDF file and return text in structured format
+    """
+    print("Parsing...")
+    data = extract_text(RAWDATA_PATH+"/"+filename)
+    break_into_chunks(data)
+
+# def extract_sections_from_text(pdf_string):
+#     """
+#     TODO
+#     """
+#     print("Extracting sections")
+#     save_to_txt()
+
+def break_into_chunks(pdf_string):
+    """
+    Breaking the pdf string into more manageable chunks
+    """
+    chunks = [pdf_string[i:i+CHUNK_SIZE] for i in range(0, len(pdf_string), CHUNK_SIZE)]
+    save_to_txt(chunks)
+
+def save_to_txt(chunks):
+    """
+    Save the parsed pdf text into a local text file
+    """
+    print("Transferring data to txt")
+    count = 0
+    with open(PROCESSED_DATA_PATH+"/"+"op.txt",'w') as file_object:
+        for chunk in chunks:
+            count+=1
+            file_object.write("Chunk "+str(count)+"\n\n")
+            file_object.write(chunk)
+            file_object.write("\n\n")
+    print("Data is ready")
+
+
+def main():
+    """
+    Main method
+    """
+    parse("11606_2022_Article_7414.pdf")
+
+
+if __name__ == "__main__":
+    main()
